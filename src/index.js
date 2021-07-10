80e0ad9b9b45e22b9b695fa653e5e3d0f75478f2
Evgenii Mizerovskii <it.mizerovskii@gmail.com>
2021-07-10 17:58:38 +0300

    Messenger 1.0.10

--- src/index.js
+++ src/index.js
@@ -1,65 +1,7 @@
 import './scss/index.scss';
 
-import { createPage as error404 } from './pages/error404';
-import { createPage as home } from './pages/home';
-import { createPage as registration } from './pages/registration';
-import { createPage as chats } from './pages/chats';
-import { createPage as user } from './pages/user';
-
-const pages = {
-  error404,
-  home,
-  registration,
-  chats,
-  user,
-};
+import { createChatStub } from '../src/modules/chatStub/chatStub.tmpl';
 
 const root = document.querySelector('#root');
 
-function renderPage(name) {
-  let template = '';
-  if (pages.hasOwnProperty(name)) {
-    template = pages[name];
-  } else {
-    template = pages['error404'];
-  }
-
-  // removeClickHandlers();
-
-  root.innerHTML = template();
-
-  // initClickHandlers();
-}
-
-const errorUrl = window.location.pathname.length;
-
-if (
-  window.location.search.toLowerCase().indexOf('error=404') >= 0 ||
-  errorUrl > 1
-) {
-  renderPage('error404');
-} else {
-  renderPage('home');
-}
-
-function initClickHandlers() {
-  const links = document.querySelectorAll('[data-page]');
-
-  for (const link of links) {
-    link.addEventListener('click', pageRenderListaner);
-  }
-}
-
-function removeClickHandlers() {
-  const links = document.querySelectorAll('[data-page]');
-
-  for (const link of links) {
-    link.removeEventListener('click', pageRenderListaner);
-  }
-}
-window.pageRenderListaner = function (ev) {
-  if (ev.hasAttribute('data-page')) {
-    renderPage(ev.getAttribute('data-page'));
-  }
-  return false;
-};
+root.innerHTML = createChatStub();
